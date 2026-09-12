e5419b938a0c622b5a55e316a1f5bbb4af5540d0
Sandro Costa e Silva <sandro.bcs@hotmail.com>
2026-09-12 01:59:56 -0300

    refactor: formatação do código

--- src/main/java/br/com/finup/dto/CategoryRequest.java
+++ src/main/java/br/com/finup/dto/CategoryRequest.java
@@ -5,6 +5,6 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
 public record CategoryRequest(
-        @NotBlank String name,
-        @NotNull CategoryType type) {
+                @NotBlank String name,
+                @NotNull CategoryType type) {
 }
