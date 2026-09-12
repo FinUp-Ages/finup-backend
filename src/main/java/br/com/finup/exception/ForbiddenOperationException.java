883bfdb41025d55939c115a1ae24006709f38523
Sandro Costa e Silva <sandro.bcs@hotmail.com>
2026-09-12 02:29:27 -0300

    refactor: mvn spotless:apply

--- src/main/java/br/com/finup/exception/ForbiddenOperationException.java
+++ src/main/java/br/com/finup/exception/ForbiddenOperationException.java
@@ -5,8 +5,8 @@ import org.springframework.http.HttpStatus;
 /**
  * Operacao nao permitida sobre o recurso. Vira 403 com corpo no formato padrao da API.
  *
- * <p>Use quando o recurso existe mas o usuario nao tem permissao para a operacao solicitada,
- * como tentar editar ou deletar uma categoria padrao do sistema.
+ * <p>Use quando o recurso existe mas o usuario nao tem permissao para a operacao solicitada, como
+ * tentar editar ou deletar uma categoria padrao do sistema.
  */
 public class ForbiddenOperationException extends BusinessException {
 
