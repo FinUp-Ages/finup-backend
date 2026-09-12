267931298a92ccfbb688f6dee0c18332cdef31c2
Sandro Costa e Silva <sandro.bcs@hotmail.com>
2026-09-12 01:20:34 -0300

    feat: cria os dtos e cria findByUserOrIsDefaultTrue no repository

---
+++ src/main/java/br/com/finup/dto/CategoryRequest.java
@@ -0,0 +1,10 @@
+package br.com.finup.dto;
+
+import br.com.finup.model.CategoryType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record CategoryRequest(
+        @NotBlank String name,
+        @NotNull CategoryType type) {
+}
